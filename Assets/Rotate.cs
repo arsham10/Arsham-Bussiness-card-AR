38e6faa8e44b5c88b9f59582a0f6f705a3b194bc
arsham10 <kidosol2256@gmail.com>
2020-04-03 12:18:25 +0800

    first commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotateSpeed; //set it in the  inspector
+    public Vector3 localEulerAngles = new Vector3(0, 180, 0);
+
+    void Update()
+    {
+        rotate();
+    }
+
+
+    void rotate()
+    {
+
+
+        transform.Rotate(new Vector3(0, 5, 0), Space.Self);
+
+
+    }
+
+}
